3b4434d0424a8459f96f6dc98ac41151f2155a24
Yuri Voinov <yvoinov@gmail.com>
2026-06-19 02:08:58 +0500

    Update profile-calloc.d
    Added elapsed time to estimate allocation frequency.

--- profile-calloc.d
+++ profile-calloc.d
@@ -1,15 +1,26 @@
-#!/usr/sbin/dtrace -s
+#!/usr/sbin/dtrace -qs
 
-    pid$1::calloc:entry
-    {
-        self->trace = 1;
-        self->size = arg0;
+#pragma D option quiet
+#pragma D option aggrate=0
 
-        @ = quantize(arg0);
-    }
+dtrace:::BEGIN
+{
+    start_ts = timestamp;
+}
 
-    pid$1::calloc:return
-    {
-        self->trace = 0;
-        self->size = 0;
-    }
+pid$1::calloc:entry
+{
+    @sizes = quantize(arg0 * arg1);
+}
+
+dtrace:::END
+{
+    printa(@sizes);
+
+    this->elapsed_sec = (timestamp - start_ts) / 1000000000;
+
+    printf("\nElapsed time: %02d:%02d:%02d\n",
+        (int)(this->elapsed_sec / 3600),
+        (int)((this->elapsed_sec % 3600) / 60),
+        (int)(this->elapsed_sec % 60));
+}
